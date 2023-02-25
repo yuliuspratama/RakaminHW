0c86d9da667ff080e33e763d50879e2a8c65b68b
Yulius Pratama <yuliuspratama9e@gmail.com>
2023-02-25 21:26:18 +0700

    Fix Row

--- HW8/Routers.js
+++ HW8/Routers.js
@@ -7,7 +7,7 @@ router.get('/infoActor',function(req,res){
         if(err){
             throw err
         }
-        res.send(rest)
+        res.status(200).json(rest.rows)
     })
 })
 
@@ -16,23 +16,25 @@ router.get('/film',function(req,res){
         if(err){
             throw err
         }
-        res.send(rest)
+        res.status(200).json(rest.rows)
     })
 })
-router.get('/film/:id',function(req,res){
-    pool.query('SELECT * FROM film WHERE film_id =' + req.params.id ,(err , rest)=>{
+router.get('/film/id/:id',function(req,res){
+    const{id} = req.params;
+    pool.query({text: 'SELECT * FROM film WHERE film_id = $1',values: [id]},(err , rest)=>{
         if(err){
             throw err
         }
-        res.send(rest)
+        res.status(200).json(rest.rows)
     })
 })
 router.get('/film/category/:category',function(req,res){
-    pool.query("SELECT * FROM film_list WHERE category  = '" + req.params.category +"'" ,(err , rest)=>{
+    const{category} = req.params;
+    pool.query({text: "SELECT * FROM film_list WHERE category  = $1", values : [category]} ,(err , rest)=>{
         if(err){
             throw err
         }
-        res.send(rest)
+        res.status(200).json(rest.rows)
     })
 })
 
@@ -41,17 +43,14 @@ router.get('/film/category/',function(req,res){
         if(err){
             throw err
         }
-        res.send(rest)
+        res.status(200).json(rest.rows)
     })
 })
 
+let pesanIndex = '<div><ul><li><a href="/film" >List Film</a></li><li><a href="/film/id/1" >Film ID 1 </a></li><li><a> ID film dapat di ubah secara manual dengan mengubah angka pada url </a></li><li><a href="/film/category/" >List category</a></li><li><a href="/film/category/Action" >Category Action</a></li><li><a>Category Action  dapat diaganti kategori lain pada URL</a></li><li><a href="/infoActor/" >List infoActor</a></li></ul></div>'
 
 router.get('/',function(req,res){
-    res.send('Get');
-})
-
-router.post('/',function(req,res){
-    res.send('post');
+    res.send(pesanIndex);
 })
 
 module.exports = router
